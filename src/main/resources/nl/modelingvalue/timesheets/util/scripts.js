6d046f9e1166ddd72a68a1948fb8801bdce97057
Tom Brus <tombrus@gmail.com>
2021-05-10 13:21:38 +0200

    add tracing

--- src/main/resources/nl/modelingvalue/timesheets/util/scripts.js
+++ src/main/resources/nl/modelingvalue/timesheets/util/scripts.js
@@ -1,12 +1,8 @@
 function setupBudgets() {
-    console.log("setupBudgets...")
     var radios = document.getElementsByTagName('input');
-    console.log("radios", radios)
     for (var r = 0; r < radios.length; r++) {
         radios[r].onclick = function () {
-            console.log("onclick radio")
             var spans = document.getElementsByTagName('span');
-            console.log("onclick radio - spans",spans)
             for (var s = 0; s <= spans.length; s++) {
                 if (spans[s] && (spans[s].classList.contains('spend') || spans[s].classList.contains('budget'))) {
                     spans[s].style.display = spans[s].classList.contains(this.id) ? 'block' : 'none';
@@ -17,7 +13,6 @@ function setupBudgets() {
             }
         }
         if (radios[r].id === 'spend') {
-            console.log("spend CLICK ", radios[r])
             radios[r].onclick();
         }
     }
